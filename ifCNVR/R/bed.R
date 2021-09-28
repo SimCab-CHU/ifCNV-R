7b089c3195b43a41667e1ec14f4bdf8b2b484a6d
SimCab-CHU <70569769+SimCab-CHU@users.noreply.github.com>
2021-09-28 16:54:42 +0200

    Changes

---
+++ ifCNVR/R/bed.R
@@ -0,0 +1,7 @@
+#' bed file for example
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 11 rows and 6 variables:
+"bed"
---
+++ ifCNVR/R/createReadsMatrix.R
@@ -0,0 +1,41 @@
+#' CreateReadsMatrix
+#'
+#' @param bamPath a path leading to the .bam and .bai files
+#' @param bedFile a path leading to the .bed file
+#' @param outputFile (optional) a path leading to a text file
+#' @param bedtools the path leading to bedtools
+#'
+#' @return a reads matrix
+#' @export
+#'
+#' @examples
+#'bamPath <- "./inst/extdata/"
+#'bed <- "./inst/bedFile.bed"
+#'bedtools <- "/Users/admin/miniconda3/bin/bedtools"
+#'readsMatrix <- CreateReadsMatrix(bamPath, bedFile, bedtools)
+#'
+CreateReadsMatrix <- function(bamPath, bedFile, bedtools, outputFile='n'){
+  bams <- dir(bamPath)
+  bed <- fread(bedFile, data.table = F)
+
+  if (sum(grepl(".bam$",bams))==0 & sum(grepl(".cram$",bams))==0){
+    stop('bamPath must contain .bam or.cram files')
+  }
+  if (sum(grepl(".bam$",bams))!=sum(grepl(".bai$",bams))){
+    stop('bamPath must contain .bam files and the correspondant index files (.bai)')
+  }
+  cat("Creating Reads Matrix\n")
+  readsMatrix <- system(paste(bedtools, "multicov -bed", bedFile, paste0("-bams ", bamPath, "/", "*.bam")),intern=TRUE)
+
+  bams <- bams[grepl(".bam$",bams)]
+  samples <- unique(unlist(strsplit(bams, split = ".bam", fixed=TRUE)))
+
+  tmp <- do.call(rbind,strsplit(readsMatrix, split="\t" ,fixed = TRUE))
+  readsMatrix <- data.frame(tmp[,4], tmp[,(ncol(bed)+1):ncol(tmp)])
+  colnames(readsMatrix) <- c("targets",samples)
+  if (outputFile!='n'){
+    fwrite(readsMatrix,outputFile,sep="\t")
+  }
+  cat("Done\n")
+  return(readsMatrix)
+}
