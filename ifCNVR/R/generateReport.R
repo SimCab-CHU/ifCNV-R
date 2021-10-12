0bd030d050fbbe5e4975ed7f341ca4db8e119a2e
SimCab-CHU <70569769+SimCab-CHU@users.noreply.github.com>
2021-10-12 19:05:49 +0200

    dev

--- ifCNVR/R/generateReport.R
+++ ifCNVR/R/generateReport.R
@@ -1,3 +1,17 @@
+#' generateReport
+#'
+#' @param readsMatrix
+#' @param abSamples
+#' @param resTable
+#' @param thrScore
+#'
+#' @return
+#' @export
+#'
+#' @examples
 generateReport <- function(readsMatrix, abSamples, resTable, thrScore=0){
+
+
+
 
 }
